dc17eef89aa8527f2d41d6f65cea1b4c754a11b7
Andrey Lapin <nethack@mail.ru>
2022-10-26 11:49:50 +0400

    refactor: change for rule: 3.4. Tag selectors

--- app.js
+++ app.js
@@ -32,19 +32,23 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.classList.add('task-item')
+
     label.innerText=taskString;
-    label.className='task';
+    label.className='task-label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.classList.add("task-item-checkbox")
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="incomplete-tasks-task task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit button";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete button";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.classList.add('delete-img')
     deleteButton.appendChild(deleteButtonImg);
 
 
